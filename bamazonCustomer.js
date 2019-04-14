d526b3d8fc6c668860b740bd19488c933cdda2c9
JDVapor <jdvapordestiny@gmail.com>
2019-04-13 23:14:28 -0700

    customer working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,64 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'Bob',
+  password: 'ArianaGrande123!',
+  database: 'bamazon'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  };
+  displayItems();
+});
+
+function displayItems() {
+  var queryStr = `SELECT * FROM products`;
+  connection.query(queryStr, function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      var data = res[i];
+      console.log(`\nProduct ID Number: ${data.id} | Name: ${data.product_name} | Type: ${data.department_name} | Cost: $${data.price} | Left in Stock: ${data.stock_qty}`);
+    };
+
+    buyProduct();
+
+  });
+};
+
+function buyProduct() {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'purchaseID',
+    message: '\nEnter the ID of the product you would like to purchase.'
+  }]).then(function(id) {
+    var queryStr = `SELECT * FROM products WHERE id = ${id.purchaseID}`;
+    connection.query(queryStr, function(err, res) {
+      inquirer.prompt([{
+        type: 'input',
+        name: 'amount',
+        message: 'How many of this product would you like to buy?'
+      }]).then(function(qty) {
+        if (qty.amount <= res[0].stock_qty) {
+          var queryStr = `UPDATE products SET stock_qty = ${res[0].stock_qty - qty.amount} WHERE id = ${id.purchaseID}`;
+          var itemBought = res[0].product_name;
+          var totalPice = qty.amount * res[0].price;
+          connection.query(queryStr, function(err, res) {
+            console.log(`You bought ${qty.amount} ${itemBought} for a total of $${totalPice}!`);
+            connection.end(function(err) {
+              if (err) {
+                console.log(err);
+              };
+            });
+          });
+        } else {
+          console.log(`Insufficient quantity! (Only ${res[0].stock_qty} left in stock!)`);
+          buyProduct();
+        };
+      });
+    })
+  });
+}
--- databaseFiles/bamazon.sql
+++ databaseFiles/bamazon.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS bamazon;
+
 CREATE DATABASE bamazon;
 
 USE bamazon;
@@ -7,8 +9,39 @@ id INT AUTO_INCREMENT not null,
 product_name VARCHAR(40) not null,
 department_name VARCHAR(25),
 price DECIMAL(10,2),
-stock_quantity INTEGER(8)
+stock_qty INTEGER(8),
+PRIMARY KEY (id)
 );
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES('Sword of Azaneth', 'Weapons', 444.44, 2); 
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES('Sword of Azaneth', 'Weapon', 444.44, 2);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES("Rageclef's Ravanger", 'Weapon', 200.25, 20);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES("Dolaz Glowing Helemt", 'Armor', 650.75, 8);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES("Rusted Boots", 'Armor', 10.50, 200);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES("Robes of Calcian", 'Armor', 333.33, 3);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES("Staff of Desire", 'Weapon', 999.99, 1);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES("Greater Healing Potion", 'Consumable', 25.00, 2000);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES("Greater Mana Potion", 'Consumable', 50.00, 2000);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES("Stone of Imortality", 'Item', 888.88, 5);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES("Crux of Calcian", 'Item', 555.55, 5);
+
+INSERT INTO products(product_name, department_name, price, stock_qty)
+VALUES("Adrog's Immolating Breastplate", 'Armor', 775.75, 7);
